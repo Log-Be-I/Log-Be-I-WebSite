9c7bc29c4e581ba5ed605877b7d5421f805661a2
YeoSeungA <silo1116@naver.cm>
2025-04-25 01:50:58 +0900

    Layout 구성

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,7 @@
-// src/pages/Dashboard.jsx
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="text-xl text-blue-600 font-bold">
+      대시보드 페이지입니다!
+    </div>
+  );
 }
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../components/layout/main";
 import Dashboard from "../pages/Dashboard";
 import UserList from "../pages/users/UserList";
 import QnaList from "../pages/qna/QnaList";
@@ -8,10 +9,12 @@ export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/users" element={<UserList />} />
-        <Route path="/qna" element={<QnaList />} />
-        <Route path="/notice" element={<NoticeList />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/qna" element={<QnaList />} />
+          <Route path="/notice" element={<NoticeList />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
