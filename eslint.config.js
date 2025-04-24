2e797f698d9e551a608d9c2452139b8b75425310
YeoSeungA <silo1116@naver.cm>
2025-04-24 19:59:29 +0900

    Taililwind, ESLint, Prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,38 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
 
 export default [
-  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    ignores: ['node_modules', 'dist'],   // 무시할 폴더
+  },
+  {
+    files: ['src/**/*.{js,jsx}'],        // 검사할 파일 범위
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react: require('eslint-plugin-react'),
+      prettier: require('eslint-plugin-prettier'),
+      tailwindcss: require('eslint-plugin-tailwindcss'),
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      // 기본 JS 규칙
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+
+      // React 관련 권장 규칙
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+
+      // Prettier 연동
+      'prettier/prettier': 'error',
+
+      // Tailwind class 정렬 검사
+      'tailwindcss/classnames-order': 'warn',
     },
   },
-]
+];
