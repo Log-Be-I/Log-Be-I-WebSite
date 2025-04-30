4f055e7307ffddb630606d1e48f3379cbf0b9e3a
YeoSeungA <silo1116@naver.cm>
2025-04-30 18:20:36 +0900

    baseURL

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export const loginApi = async ({ username, password }) => {
   console.log("🔥 로그인 요청 데이터:", { username, password });
   const response = await axios.post(
-    "http://localhost:8080/auth/login",
+    "https://web.logbe-i.com/auth/login",
     { username, password },
     { headers: { "Content-Type": "application/json" } }
   );
--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080", // 👉 실제 API 주소로 변경
+  baseURL: "https://web.logbe-i.com", // 👉 실제 API 주소로 변경
   timeout: 5000,
 });
 
