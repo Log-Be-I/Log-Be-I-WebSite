eacf78f13d7bca68e55f90beddb1acce74912160
YeoSeungA <silo1116@naver.cm>
2025-05-01 11:01:34 +0900

    아이콘 수정

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/auth": {
-<<<<<<< HEAD
-        target: "http://localhost:8080",
-=======
-        target: "https://logbe-i.com",
->>>>>>> e40ad4ce3e68c5c2c67c1fc6f66ce552ac7dd4b3
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/auth": {
+  //       target: "https://logbe-i.com",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 });
