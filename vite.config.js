1e08abb19cd0a6c93eb9fad3a68510dd1ec6ceef
NamTaekHo <taekho1225@gmail.com>
2025-05-07 15:46:58 +0900

    config에 경로 추가

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,14 @@ export default defineConfig({
         target: "https://logbe-i.com",
         changeOrigin: true,
         secure: false,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        privacy: path.resolve(__dirname, "public/privacy.html"),
+        terms: path.resolve(__dirname, "public/terms.html"),
       },
     },
   },
