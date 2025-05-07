ae27791c4348c448e50e6126995f27e5a470c228
NamTaekHo <taekho1225@gmail.com>
2025-05-07 15:51:51 +0900

    config 수정

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
+        main: path.resolve(__dirname, "index.html"),
         privacy: path.resolve(__dirname, "public/privacy.html"),
         terms: path.resolve(__dirname, "public/terms.html"),
       },
